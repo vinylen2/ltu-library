9592cbf279f3e19305257e8d692e1505a411b574
lilGandhi <50099733+lilGandhi@users.noreply.github.com>
2019-05-02 20:08:16 +0200

    asdasd

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-			
+		System.out.println("asdasd");
+		System.out.println("asdasd");
+		System.out.println("asdasd");
 	}
 }
