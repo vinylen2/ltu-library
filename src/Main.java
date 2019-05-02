5253a0ac58961e9b11d036c371b63c5b2fd78788
vinylen2 <gabrielwallen93@gmail.com>
2019-05-02 20:11:07 +0200

    new stuff

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import views.MainFrame;
+import views.MainFrame2;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-			
+		new MainFrame2();
 	}
 }
---
+++ src/views/MainFrame.java
@@ -0,0 +1,13 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class MainFrame {
+	JFrame frame;
+	
+	public MainFrame() {
+		frame = new JFrame("Library");
+		frame.setSize(500, 300);
+		frame.setVisible(true);
+	}
+}
