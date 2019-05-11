0f6654be1afc649425b9a1584fcab7b3889a0383
lilGandhi <50099733+lilGandhi@users.noreply.github.com>
2019-05-11 15:21:37 +0200

    lagt till addOjbectView och AddBorrowerView

---
+++ src/view/AddBorrowerView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class AddBorrowerView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AddBorrowerView() {
+
+	}
+
+}
---
+++ src/view/AddObjectView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class AddObjectView extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AddObjectView() {
+
+	}
+
+}
